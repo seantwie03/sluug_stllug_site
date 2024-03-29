2ce97e442d583b6de72b49a1b899a7b1aca9502f
Sean Twie03 <sean.twie03@mail.com>
2024-03-29 05:40:23 -0500

    feat: view transitions

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const sluugMeetings = await getCollection('sluug');
     return rss({
         title: 'St. Louis Unix/Linux Users Group',
         description: 'My journey learning Astro',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+        items: sluugMeetings.map((meeting) => ({
+            title: meeting.data.title,
+            pubDate: meeting.data.pubDate,
+            description: meeting.data.description,
+            link: `/sluug/${meeting.slug}/`,
+        })),
         customData: `<language>en-us</language>`,
     });
 }
