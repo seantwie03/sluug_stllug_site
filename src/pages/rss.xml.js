800a5f082454d0948fe9f795dc02592b2d1468d8
Sean Twie03 <sean.twie03@mail.com>
2024-04-01 18:27:47 -0500

    fix: fix import

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { sortMeetingListByMeetingDate } from '../../buildTimeUtils';
+import { sortMeetingListByMeetingDate } from '../buildTimeUtils';
 
 export async function GET(context) {
     const allMeetings = sortMeetingListByMeetingDate(await getCollection('meetings'));
