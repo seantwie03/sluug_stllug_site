6613ba2507ff2c325d82a77d0d48aed28bc45e7d
Sean Twie03 <sean.twie03@mail.com>
2024-04-06 10:48:27 -0500

    feat: improve header and images

--- src/buildTimeUtils.ts
+++ src/buildTimeUtils.ts
@@ -1,16 +1,28 @@
-import type { CollectionEntry } from 'astro:content';
-import { type MeetingType, type Image } from './models';
+import type { CollectionEntry } from "astro:content";
+import { type Image, type MeetingType } from "./models";
 
 export function getMeetingName(meetingType: MeetingType) {
-    return meetingType === 'SLUUG' ? `St. Louis Unix Users Group` : `St. Louis Linux Users Group`;
+  return meetingType === "SLUUG"
+    ? `St. Louis Unix Users Group`
+    : `St. Louis Linux Users Group`;
 }
 
 export function getMeetingImage(meetingType: MeetingType): Image {
-    return meetingType === 'SLUUG'
-        ? { url: '/SLUUG-Logo.png', alt: 'St. Louis Unix Users Group Logo' }
-        : { url: '/STLLUG-Logo.png', alt: 'St. Louis Linux Users Group Logo' };
+  return meetingType === "SLUUG"
+    ? {
+        url: "/src/images/SLUUG-Logo.png",
+        alt: "St. Louis Unix Users Group Logo",
+      }
+    : {
+        url: "/src/images/STLLUG-Logo.png",
+        alt: "St. Louis Linux Users Group Logo",
+      };
 }
 
-export function sortMeetingListByMeetingDate(meetings: CollectionEntry<'meetings'>[]) {
-    return meetings.sort((a, b) => b.data.meetingDate.getTime() - a.data.meetingDate.getTime());
+export function sortMeetingListByMeetingDate(
+  meetings: CollectionEntry<"meetings">[],
+) {
+  return meetings.sort(
+    (a, b) => b.data.meetingDate.getTime() - a.data.meetingDate.getTime(),
+  );
 }
