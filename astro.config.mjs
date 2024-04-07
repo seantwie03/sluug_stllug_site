d8dbf272356947c08d7666c33714df4c6c709916
Sean Twie03 <sean.twie03@mail.com>
2024-04-07 07:34:56 -0500

    feat: add site map

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-
 import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://main--sparkly-alfajores-b08ac3.netlify.app/',
   integrations: [mdx(), tailwind({
     applyBaseStyles: false
-  }), icon()]
+  }), icon(), sitemap()]
 });
