4f6171a63a6f6876678ed55dd3d0344b1cbdaa5d
Sean Twie03 <sean.twie03@mail.com>
2024-04-10 04:17:00 -0500

    feat: paginate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import icon from "astro-icon";
@@ -8,6 +8,11 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://main--sparkly-alfajores-b08ac3.netlify.app/',
+  redirects: {
+    "/": "/meetings/1",
+    "/sluug": "/meetings/sluug/1",
+    "/stllug": "/meetings/stllug/1",
+  },
   integrations: [mdx(), tailwind({
     applyBaseStyles: false
   }), icon(), sitemap()]
