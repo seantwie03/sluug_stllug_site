04743e2582ddec150f1e7a7d4e5ee355ab3f3d2a
Sean Twie03 <sean.twie03@mail.com>
2024-04-21 07:38:16 -0500

    feat: enable multiple presenters on a single presentation

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,37 +1,36 @@
 import { defineCollection, z } from "astro:content";
 
 const meetingCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    meetingType: z.enum(["SLUUG", "STLLUG"]),
-    meetingDate: z.date(),
-    main: z.object({
-      title: z.string(),
-      presenter: z.string(),
-      tags: z.array(z.string()),
+    type: "content",
+    schema: z.object({
+        meetingType: z.enum(["SLUUG", "STLLUG"]),
+        meetingDate: z.date(),
+        main: z.object({
+            title: z.string(),
+            presenterNames: z.array(z.string()),
+            tags: z.array(z.string()),
+        }),
+        base: z
+            .object({
+                title: z.string(),
+                presenterNames: z.array(z.string()),
+                tags: z.array(z.string()),
+            })
+            .optional(),
+        youtubeUrl: z.string().optional(),
+        meetupUrl: z.string().optional(),
     }),
-    base: z
-      .object({
-        title: z.string(),
-        presenter: z.string(),
-        tags: z.array(z.string()),
-      })
-      .optional(),
-    youtubeUrl: z.string().optional(),
-    meetupUrl: z.string().optional(),
-  }),
 });
 
 const presenterCollection = defineCollection({
-  type: "content",
-  schema: z.object({
-    presenterName: z.string(),
-    lastUpdated: z.date(),
-    links: z.array(z.string()),
-  }),
+    type: "content",
+    schema: z.object({
+        presenterName: z.string(),
+        lastUpdated: z.date(),
+    }),
 });
 
 export const collections = {
-  meetings: meetingCollection,
-  presenters: presenterCollection,
+    meetings: meetingCollection,
+    presenters: presenterCollection,
 };
--- src/models.ts
+++ src/models.ts
@@ -1,14 +1,14 @@
 import type { ImageMetadata } from "astro";
 
 export interface Presentation {
-  title: string;
-  presenter: string;
-  tags: string[];
+    title: string;
+    presenterNames: string[];
+    tags: string[];
 }
 
 export type MeetingType = "SLUUG" | "STLLUG";
 
 export interface Image {
-  src: ImageMetadata;
-  alt: string;
+    src: ImageMetadata;
+    alt: string;
 }
