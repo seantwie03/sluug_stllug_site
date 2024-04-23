5ad209aefeb42f619b616ff58f06699cc5c68325
Sean Twie03 <sean.twie03@mail.com>
2024-04-23 07:50:48 -0500

    chore: imports and formatting

--- src/buildTimeUtils.ts
+++ src/buildTimeUtils.ts
@@ -1,31 +1,31 @@
 import type { CollectionEntry } from "astro:content";
-import { type Image, type MeetingType } from "./models";
-import sluugLogo from "./images/SLUUG-Logo.png";
-import stllugLogo from "./images/STLLUG-Logo.png";
+import { type Image, type MeetingType } from "@/models";
+import sluugLogo from "@/images/SLUUG-Logo.png";
+import stllugLogo from "@/images/STLLUG-Logo.png";
 import { SLUUG_LOGO_ALT, STLLUG_LOGO_ALT } from "@/constants.ts";
 
 export function getMeetingName(meetingType: MeetingType) {
-  return meetingType === "SLUUG"
-    ? `St. Louis Unix Users Group`
-    : `St. Louis Linux Users Group`;
+    return meetingType === "SLUUG"
+        ? `St. Louis Unix Users Group`
+        : `St. Louis Linux Users Group`;
 }
 
 export function getMeetingImage(meetingType: MeetingType): Image {
-  return meetingType === "SLUUG"
-    ? {
-        src: sluugLogo,
-        alt: SLUUG_LOGO_ALT,
-      }
-    : {
-        src: stllugLogo,
-        alt: STLLUG_LOGO_ALT,
-      };
+    return meetingType === "SLUUG"
+        ? {
+              src: sluugLogo,
+              alt: SLUUG_LOGO_ALT,
+          }
+        : {
+              src: stllugLogo,
+              alt: STLLUG_LOGO_ALT,
+          };
 }
 
 export function sortMeetingListByMeetingDate(
-  meetings: CollectionEntry<"meetings">[],
+    meetings: CollectionEntry<"meetings">[],
 ) {
-  return meetings.sort(
-    (a, b) => b.data.meetingDate.getTime() - a.data.meetingDate.getTime(),
-  );
+    return meetings.sort(
+        (a, b) => b.data.meetingDate.getTime() - a.data.meetingDate.getTime(),
+    );
 }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,14 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { sortMeetingListByMeetingDate } from '../buildTimeUtils';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { sortMeetingListByMeetingDate } from "@/buildTimeUtils";
 
 export async function GET(context) {
-    const allMeetings = sortMeetingListByMeetingDate(await getCollection('meetings'));
+    const allMeetings = sortMeetingListByMeetingDate(
+        await getCollection("meetings"),
+    );
     return rss({
-        title: 'St. Louis Unix/Linux Users Group',
-        description: 'RSS Feed of our monthly SLUUG and STLLUG meetings',
+        title: "St. Louis Unix/Linux Users Group",
+        description: "RSS Feed of our monthly SLUUG and STLLUG meetings",
         site: context.site,
         items: allMeetings.map((meeting) => ({
             title: meeting.slug,
