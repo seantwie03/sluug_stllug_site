82d2f43e7a08bd5c4814c20bea365a098ad58e1d
Sean Twie03 <sean.twie03@mail.com>
2024-04-23 07:43:26 -0500

    feat: consolidate types

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,47 +1,42 @@
 import { defineCollection, z } from "astro:content";
 
+export const MeetingTypeZod = z.enum(["SLUUG", "STLLUG"]);
+export const PresentationZod = z.object({
+    title: z.string(),
+    presenterNames: z.array(z.string()),
+    tags: z
+        .array(z.string())
+        .describe(
+            "The name of the presenter(s) formatted how it should be displayed on the website. Each item in this array must match a presenterName in the presenterCollection for the Presenter links on the Meeting pages to work.",
+        ),
+});
+export const MeetingSchemaZod = z.object({
+    meetingType: MeetingTypeZod,
+    meetingDate: z.date(),
+    main: PresentationZod,
+    base: PresentationZod.optional(),
+    youtubeUrl: z.string().optional(),
+    meetupUrl: z.string().optional(),
+});
 const meetingCollection = defineCollection({
     type: "content",
-    schema: z.object({
-        meetingType: z.enum(["SLUUG", "STLLUG"]),
-        meetingDate: z.date(),
-        main: z.object({
-            title: z.string(),
-            presenterNames: z.array(z.string()),
-            tags: z
-                .array(z.string())
-                .describe(
-                    "The name of the presenter(s) formatted how it should be displayed on the website. Each item in this array must match a presenterName in the presenterCollection for the Presenter links on the Meeting pages to work.",
-                ),
-        }),
-        base: z
-            .object({
-                title: z.string(),
-                presenterNames: z
-                    .array(z.string())
-                    .describe(
-                        "The name of the presenter(s) formatted how it should be displayed on the website. Each item in this array must match a presenterName in the presenterCollection for the Presenter links on the Meeting pages to work.",
-                    ),
-                tags: z.array(z.string()),
-            })
-            .optional(),
-        youtubeUrl: z.string().optional(),
-        meetupUrl: z.string().optional(),
-    }),
+    schema: MeetingSchemaZod,
+});
+
+export const PresenterSchemaZod = z.object({
+    presenterName: z
+        .string()
+        .describe(
+            "The presenter's name formatted how it should be displayed on the website. Example: George R.R. Martin",
+        ),
+    lastUpdated: z
+        .date()
+        .describe("The date when the Presenter's file was last updated."),
 });
 
 const presenterCollection = defineCollection({
     type: "content",
-    schema: z.object({
-        presenterName: z
-            .string()
-            .describe(
-                "The presenter's name formatted how it should be displayed on the website. Example: George R.R. Martin",
-            ),
-        lastUpdated: z
-            .date()
-            .describe("The date when the Presenter's file was last updated."),
-    }),
+    schema: PresenterSchemaZod,
 });
 
 export const collections = {
--- src/models.ts
+++ src/models.ts
@@ -1,12 +1,16 @@
 import type { ImageMetadata } from "astro";
+import type { z } from "astro/zod";
+import type {
+    MeetingSchemaZod,
+    MeetingTypeZod,
+    PresentationZod,
+    PresenterSchemaZod,
+} from "@/content/config";
 
-export interface Presentation {
-    title: string;
-    presenterNames: string[];
-    tags: string[];
-}
-
-export type MeetingType = "SLUUG" | "STLLUG";
+export type MeetingType = z.infer<typeof MeetingTypeZod>;
+export type Presentation = z.infer<typeof PresentationZod>;
+export type Meeting = z.infer<typeof MeetingSchemaZod>;
+export type Presenter = z.infer<typeof PresenterSchemaZod>;
 
 export interface Image {
     src: ImageMetadata;
