230c39a668597f37f4061ef161489cf5c9c5520b
Sean Twie03 <sean.twie03@mail.com>
2024-07-16 06:43:54 -0500

    feat: change site name ; add mastodon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
-import {defineConfig} from 'astro/config';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://main--sparkly-alfajores-b08ac3.netlify.app/',
-  redirects: {
-    "/": "/meetings/1",
-    "/sluug": "/meetings/sluug/1",
-    "/stllug": "/meetings/stllug/1",
-  },
-  integrations: [mdx(), tailwind({
-    applyBaseStyles: false
-  }), icon(), sitemap()]
+    site: "https://sluug.netlify.app/",
+    redirects: {
+        "/": "/meetings/1",
+        "/sluug": "/meetings/sluug/1",
+        "/stllug": "/meetings/stllug/1",
+    },
+    integrations: [
+        mdx(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        icon(),
+        sitemap(),
+    ],
 });
