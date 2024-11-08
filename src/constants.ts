c46e0c88405bc173cc1ef7e0309006d643aec83c
Sean Twie03 <sean.twie03@mail.com>
2024-11-08 06:23:25 -0600

    feat: address user feedback

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const SLUUG_LOGO_ALT =
-    "The St. Louis Unix Users Group Logo is a moderately complex illustration of the Gateway Arch above a city skyline where the buildings have been made to look like server hardware. The letters S-L-U-U-G in a banner at the bottom.";
+    "The St. Louis Unix Users Group Logo features the Gateway Arch with the name of the group.";
 export const STLLUG_LOGO_ALT =
-    "The St. Louis Linux Users Group Logo is a relatively simple illustration of the Linux Penguin beneath the Gateway Arch. The letters S-T-L-L-U-G appear in a banner at the bottom.";
+    "The St. Louis Linux Users Group Logo features the Linux Penguin beneath the Gateway Arch. The letters S-T-L-L-U-G appear in a banner at the bottom.";
 export const pageSize = 6;
 
 export const SLUUG_SITE_URL = "https://www.sluug.org";
